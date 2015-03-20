a44862210baaecf168e511f70ee39f3d93702116
Benjamin Clay <bclay@jolicode.com>
2015-03-20 22:53:24 +0100

    Add some cache

--- web/app.php
+++ web/app.php
@@ -2,29 +2,48 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 $app = new Silex\Application();
-$app['debug'] = true;
+//$app['debug'] = true;
 
-$app['weekend'] = function () {
-    return new \Ternel\Weekend();
-};
-
+// Twig
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
+// Cache
+$app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
+    'http_cache.cache_dir' => __DIR__.'/../cache/',
+));
 
+// Weekend, where the magic happens.
+$app['weekend'] = function () {
+    return new \Ternel\Weekend();
+};
+
+
+// Website
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig', array(
-        'text'    => $app['weekend']->getText(),
-        'subtext' => $app['weekend']->getSubText(),
-    ));
+    return new Response($app['twig']->render('index.html.twig', [
+            'text'    => $app['weekend']->getText(),
+            'subtext' => $app['weekend']->getSubText(),
+        ]),
+        200,
+        ['Cache-Control' => 's-maxage=60']
+    );
 });
 
+// Api
 $app->get('/api', function () use ($app) {
-    return $app->json([
-        'text'    => $app['weekend']->getText(),
-        'subtext' => $app['weekend']->getSubText(),
-    ]);
+    return new JsonResponse([
+            'text'    => $app['weekend']->getText(),
+            'subtext' => $app['weekend']->getSubText(),
+        ],
+        200,
+        ['Cache-Control' => 'maxage=60']
+    );
 });
-$app->run();
+
+$app['http_cache']->run();
